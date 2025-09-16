c1f2a0975be1609873256f7554b140bdcad21598
Janitha Ranasinghe <janitha479@gmail.com>
2025-09-16 15:27:21 +0530

    Update ManageCourses.jsx

--- src/Pages/LMS/ManageCourses.jsx
+++ src/Pages/LMS/ManageCourses.jsx
@@ -416,7 +416,8 @@ const ManageCourses = ({ onViewCourse }) => {
             </div>
 
             <div className="text-xs text-gray-400 mb-4">
-              Created by: {LMSService.getCourseCreator(course.id)?.name || "Unknown"}
+              Created by:{" "}
+              {LMSService.getCourseCreator(course.id)?.name || "Unknown"}
             </div>
 
             <div className="flex space-x-2">
