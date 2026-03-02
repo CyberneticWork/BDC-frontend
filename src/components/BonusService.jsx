2be482acd18d9463b805c985c2efc2dff30fc22a
Chamod-alt <152728704+Chamod-alt@users.noreply.github.com>
2026-03-02 22:57:10 +0530

    add to salary process bonus part

---
+++ src/components/BonusService.jsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "http://127.0.0.1:8000/api",
+  // withCredentials: true, // needed only if you use cookie auth
+});
+
+// If you use Sanctum token:
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+const BonusService = {
+  async getAllBonuses() {
+    const res = await API.get("/bonuses");
+    // Laravel often returns: { data: [...] } OR just [...]
+    return Array.isArray(res.data) ? res.data : (res.data?.data ?? []);
+  },
+
+  async getBonusesByCompanyOrDepartment(companyId, departmentId) {
+    const res = await API.get("/bonuses/by-company-or-department", {
+      params: { company_id: companyId, department_id: departmentId },
+    });
+    return Array.isArray(res.data) ? res.data : (res.data?.data ?? []);
+  },
+};
+
+export default BonusService;
